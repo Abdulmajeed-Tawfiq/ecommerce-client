eca7f52ca89e9bef564951036b9ec9f0bed31a52
Abdulmajeed-Tawfiq <abdulmajeedtawfiq98@gmail.com>
2024-03-06 08:33:32 +0300

    solve some bugs

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import useFetch from "../../hooks/useFetch";
 import Card from "../card/Card";
 import styles from "./List.module.css";
@@ -6,7 +7,7 @@ import styles from "./List.module.css";
 https://docs.strapi.io/dev-docs/api/rest/filters-locale-publication#filtering */
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-function List({ catId, subCats }: { catId: any; subCats: any }) {
+function List({ catId, subCats, sort }: { catId: any; subCats: any; sort: any }) {
   const { data, loading } = useFetch(
     `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
       (item: object) => `&[filters][sub_categories][id][$eq]=${item}`
